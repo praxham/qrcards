ccb54899ea1aba756171e61d8bd7ebd5f58b57fb
Pratham Patankar <83165113+prathampatankar@users.noreply.github.com>
2023-05-04 01:14:43 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,33 @@
-let cacheVersion = 1
-let cacheName = "web-workr-cache-"+cacheVersion
-const pageToSave = "index.html"
-const pageToSave2 = "add.html"
-const imgToSave = "Aadhar-Color.svg"
-const imgToSave2 = "add-symbol-svgrepo-com.svg"
-const imgToSave3 = "copy.svg"
-const imgToSave4 = "deleteicon.svg"
-const imgToSave5 = "Electionlogo.svg"
-const imgToSave6 = "Income-Tax-Department-Color.svg"
-const imgToSave7 = "link.svg"
-const imgToSave8 = "qrcardslogo.png"
-const imgToSave9 = "UPI-Color.svg"
-
-// Installing service worker
-this.addEventListener('install', event => {
-    console.log("Installing service worker");
-  
-    event.waitUntil(caches.open(cacheName)
-    .then((openCache) => {
-        return openCache.add(pageToSave)
-        return openCache.add(pageToSave2)
-    })
-    .catch(err => console.log(err)))
+const CACHE_NAME = "static_cache"
+const STATIC_ASSETS = [
+    '/index.html',
+    '/add.html',
+    '/manifest.json',
+    '/qrcardslogo.png'
+]
+async function preCache() {
+    const cache = await caches.open(CACHE_NAME)
+    return cache.addAll(STATIC_ASSETS)
+}
+self.addEventListener('install',event => {
+    console.log('installed');
+    event.waitUntil(preCache())
 })
-self.addEventListener('activate', e => {
-    console.log('Activation!');
-    });
-    this.addEventListener('fetch', event => {
-        console.log("Fetching with service worker");
-        if(event.request.mode === 'navigate'){
-            event.respondWith(
-                fetch(event.request.url)
-                .catch(_ => {
-                    return caches.match(pageToSave)
-                })
-            )
-        }
-    })
+self.addEventListener('activate',event => {
+    console.log('installed');
+})
+async function fetchAssets(event) {
+    try {
+        const response = await fetch(event.request)
+        return response
+    } catch (err) {
+        const cache = await caches.open(CACHE_NAME)
+        return cache.match(event.request)
+    }
+}   
+
+self.addEventListener('fetch',event => {
+    console.log('fetched');
+    event.respondWith(fetchAssets(event))
+
+})
