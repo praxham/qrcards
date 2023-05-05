c791fd5582281e7a8bba15b4f8c37ae8f0b1c356
Pratham Patankar <83165113+prathampatankar@users.noreply.github.com>
2023-05-05 22:09:49 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,12 @@
 const CACHE_NAME = "static_cache"
 const STATIC_ASSETS = [
-    '/index.html',
-    '/add.html',
-    '/manifest.json',
-    '/qrcardslogo.png',
-    '/html5qrcodelib.js'
+    'index.html',
+    'add.html',
+    'manifest.json',
+    'qrcardslogo.png',
+    'html5qrcodelib.js',
+    'jqueryimgcaching.js',
+    'Sortable.js'
 ]
 async function preCache() {
     const cache = await caches.open(CACHE_NAME)
@@ -12,10 +14,21 @@ async function preCache() {
 }
 self.addEventListener('install',event => {
     console.log('installed');
+    self.skipWaiting()
     event.waitUntil(preCache())
 })
+async function cleanupCache() {
+    const keys = await caches.keys()
+    const keysToDelete = keys.map(key => {
+        if(key !== CACHE_NAME) {
+            return caches.delete(key)
+        }
+    })
+    return Promise.all(keysToDelete)
+}
 self.addEventListener('activate',event => {
     console.log('installed');
+    event.waitUntil(cleanupCache())
 })
 async function fetchAssets(event) {
     try {
@@ -26,9 +39,7 @@ async function fetchAssets(event) {
         return cache.match(event.request)
     }
 }   
-
 self.addEventListener('fetch',event => {
     console.log('fetched');
     event.respondWith(fetchAssets(event))
-
 })
